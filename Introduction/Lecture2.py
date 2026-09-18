6eec2b06c02176119d74875066c57913c5e089c8
Officialritesh45 <riteshpal861@gmail.com>
2026-09-19 02:18:23 +0530

    Lecture2.py

---
+++ Introduction/Lecture2.py
@@ -0,0 +1,14 @@
+# variable :- Variable is a name given to a memory location that stores data
+# python explicit (Don't need to declare variable type)
+name= "Ritesh pal"#name is the variable and "Ritesh pal" is the value assigned to the variable
+print(name)# string variable
+print(type(name))#print type of variable
+age =22
+print(age)#integer variable
+print(type(age))
+Course = "AI Era"
+print(Course)
+print(type(Course))
+CGPA=7.3#CGPA is the variable and 7.3 is the value assigned to the variable
+print(CGPA)#float variable
+print(type(CGPA))
