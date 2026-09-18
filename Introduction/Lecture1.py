0d60234a5b0f4400de302eea928ee97490bdbd79
Officialritesh45 <riteshpal861@gmail.com>
2026-09-19 00:07:48 +0530

    Lecture1.py

--- Introduction/Lecture1.py
+++ Introduction/Lecture1.py
@@ -1,1 +1,7 @@
-
+"""
+Code By- Ritesh Pal
+Python is High-level programming language. It is used for web development, data analysis, artificial intelligence, scientific computing.
+python is an interpreted (Line by Line execution) language
+"""
+#First Program
+print("Hello AI Era Developer ----")
