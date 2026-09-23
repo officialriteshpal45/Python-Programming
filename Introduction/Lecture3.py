f9ff5350ab3ddf6c24087cf94e201e4fe6be9b77
Officialritesh45 <riteshpal861@gmail.com>
2026-09-24 02:12:35 +0530

    Lecture3.py

---
+++ Introduction/Lecture3.py
@@ -0,0 +1,12 @@
+#Data type in python :- Data type is an attribute of data which tells the interpreter how the programmer intends to use the data
+#there are various data types in python like int, float, complex, string, list, tuple, set, dictionary etc
+#int is store whole number without decimal point
+Name = "Ritesh Pal"
+print(type(Name))#string data type
+Roll_no = 101
+print(type(Roll_no))#integer data type
+Pass =True
+print(type(Pass))#boolean data type
+CGPA = 7.3
+print(type(CGPA))#float data type
+#These are the basic data types in python
---
+++ Operator/lecture4.py
@@ -0,0 +1,9 @@
+# input & output in python language 
+# 1. Take input with user 
+# EXAMPLE : ADD Two number 
+A= int(input("Enter the First Num :--"))
+B= int(input("Enter the First Num :--"))
+
+sum = A + B
+print("Sum of 2 num in :--",sum)
+ 
